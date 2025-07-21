9109ac71b0c07b177b42d01fb202a65d8f7ebe36
Josh Beatty <beattyjoshua55@Gmail.com>
2025-07-21 21:33:38 +0100

    Ejected application to regular react native project because mapping options arent supported by expo

--- AutoSAR/app/mainPage.tsx
+++ AutoSAR/app/mainPage.tsx
@@ -1,19 +1,27 @@
-import { StyleSheet, Text, View } from "react-native";
+import MapboxGL from '@rnmapbox/maps';
+import { View, StyleSheet } from 'react-native';
+
+MapboxGL.setAccessToken(process.env.MAPBOX_ACCESS_TOKEN);
 
 export default function MainPage() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello Josh.</Text>
+    <View style={styles.page}>
+      <MapboxGL.MapView style={styles.map}>
+        <MapboxGL.Camera
+          zoomLevel={14}
+          centerCoordinate={[-1.93, 53.8]} // Example location
+        />
+
+        {/* Add IPP pin */}
+        <MapboxGL.PointAnnotation id="ipp" coordinate={[-1.93, 53.8]} />
+
+        {/* Add range rings and sectors here */}
+      </MapboxGL.MapView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
-})
+  page: { flex: 1 },
+  map: { flex: 1 }
+});
