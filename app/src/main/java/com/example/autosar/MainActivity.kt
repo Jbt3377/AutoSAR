ece06191399e478f13cbaa4caaf03ad1fa385208
Josh Beatty <beattyjoshua55@Gmail.com>
2025-09-04 22:52:08 +0100

    Added GPS button

--- app/src/main/java/com/example/autosar/MainActivity.kt
+++ app/src/main/java/com/example/autosar/MainActivity.kt
@@ -3,11 +3,19 @@ package com.example.autosar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.mapbox.geojson.Point
 import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
@@ -31,15 +39,26 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MapboxMapScreen() {
-    MapboxMap(
-        Modifier.fillMaxSize(),
-        mapViewportState = rememberMapViewportState {
-            setCameraOptions {
-                zoom(2.0)
-                center(Point.fromLngLat(-98.0, 39.5))
-                pitch(0.0)
-                bearing(0.0)
-            }
-        },
-    )
+    Box(Modifier.fillMaxSize()) {
+        MapboxMap(
+            Modifier.fillMaxSize(),
+            mapViewportState = rememberMapViewportState {
+                setCameraOptions {
+                    zoom(2.0)
+                    center(Point.fromLngLat(-98.0, 39.5))
+                    pitch(0.0)
+                    bearing(0.0)
+                }
+            },
+        )
+
+        FloatingActionButton(
+            onClick = { },
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp)
+        ) {
+            Icon(Icons.Filled.Place, "Floating action button.")
+        }
+    }
 }
