3eb8065301322ff741f32eed8469a871cf43a9c7
Josh Beatty <beattyjoshua55@Gmail.com>
2025-09-18 22:21:20 +0100

    Added crosshair

--- app/src/main/java/com/example/autosar/MainActivity.kt
+++ app/src/main/java/com/example/autosar/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.material3.FloatingActionButton
@@ -23,13 +24,16 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import com.example.autosar.models.LocationViewModel
 import com.mapbox.geojson.Point
 import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
+import com.example.autosar.R
 
 class MainActivity : ComponentActivity() {
 
@@ -108,6 +112,15 @@ fun MapboxMapScreen(locationViewModel: LocationViewModel) {
         MapboxMap(
             Modifier.fillMaxSize(),
             mapViewportState = mapViewportState
+        )
+
+        Icon(
+            painter = painterResource(id = R.drawable.crosshair),
+            contentDescription = "Map crosshair",
+            tint = Color.Black, // Adjust color as needed
+            modifier = Modifier
+                .align(Alignment.Center)
+                .size(20.dp)
         )
 
         if (hasPermission) {
