6e89b0f49f0096fcf9c448001267bc16137806d2
Josh Beatty <beattyjoshua55@Gmail.com>
2025-09-21 19:59:27 +0100

    Add Marker on Long Press (#4)
    * Added crosshair
    
    * Minor refactor
    
    * Marker rendering
    
    * Updated icon and size - now working on long click

--- app/src/main/java/com/example/autosar/MainActivity.kt
+++ app/src/main/java/com/example/autosar/MainActivity.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
@@ -11,6 +12,7 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.material3.FloatingActionButton
@@ -23,17 +25,27 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import com.example.autosar.models.LocationViewModel
+import com.example.autosar.models.MarkerViewModel
 import com.mapbox.geojson.Point
+import com.mapbox.maps.Style
 import com.mapbox.maps.extension.compose.MapboxMap
 import com.mapbox.maps.extension.compose.animation.viewport.rememberMapViewportState
+import com.mapbox.maps.extension.compose.annotation.generated.PointAnnotation
+import com.mapbox.maps.extension.compose.annotation.generated.PointAnnotationGroup
+import com.mapbox.maps.extension.compose.annotation.rememberIconImage
+import com.mapbox.maps.extension.compose.style.MapStyle
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
 
 class MainActivity : ComponentActivity() {
 
     private val locationViewModel: LocationViewModel by viewModels()
+    private val markerViewModel: MarkerViewModel by viewModels()
 
     @SuppressLint("MissingPermission")
     private val requestPermissionLauncher =
@@ -47,14 +59,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Check if we already have permission
         val granted = ContextCompat.checkSelfPermission(
             this,
             Manifest.permission.ACCESS_FINE_LOCATION
         ) == PackageManager.PERMISSION_GRANTED
 
         locationViewModel.setPermissionGranted(granted)
-
         if (!granted) {
             requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
         } else {
@@ -62,39 +72,42 @@ class MainActivity : ComponentActivity() {
         }
 
         setContent {
-            AppContent(locationViewModel)
+            AppContent(locationViewModel, markerViewModel)
         }
     }
 }
 
 @Composable
-fun AppContent(locationViewModel: LocationViewModel) {
+fun AppContent(locationViewModel: LocationViewModel, markerViewModel: MarkerViewModel) {
     MaterialTheme {
         Surface(
             modifier = Modifier.fillMaxSize(),
             color = MaterialTheme.colorScheme.background
         ) {
-            MapboxMapScreen(locationViewModel)
+            MapboxMapScreen(locationViewModel, markerViewModel)
         }
     }
 }
 
 @SuppressLint("MissingPermission")
 @Composable
-fun MapboxMapScreen(locationViewModel: LocationViewModel) {
+fun MapboxMapScreen(
+    locationViewModel: LocationViewModel,
+    markerViewModel: MarkerViewModel
+) {
     val context = LocalContext.current
-
     val mapViewportState = rememberMapViewportState {
         setCameraOptions {
             zoom(2.0)
-            center(Point.fromLngLat(-98.0, 39.5)) // fallback center
+            center(Point.fromLngLat(-98.0, 39.5))
         }
     }
 
     val userLocation by locationViewModel.userLocation.collectAsState()
     val hasPermission by locationViewModel.hasPermission.collectAsState()
+    val markers by markerViewModel.markers.collectAsState()
 
-    // When location updates, move the camera
+    // move camera when location updates
     LaunchedEffect(userLocation) {
         userLocation?.let { point ->
             mapViewportState.setCameraOptions {
@@ -106,8 +119,34 @@ fun MapboxMapScreen(locationViewModel: LocationViewModel) {
 
     Box(Modifier.fillMaxSize()) {
         MapboxMap(
-            Modifier.fillMaxSize(),
-            mapViewportState = mapViewportState
+            modifier = Modifier.fillMaxSize(),
+            mapViewportState = mapViewportState,
+            style = { MapStyle(style = Style.OUTDOORS) },
+            onMapLongClickListener = { point ->
+                markerViewModel.addMarker(point)
+                true
+            }
+        ) {
+            Log.d("MapboxMapScreen", "Markers: $markers")
+
+            val marker = rememberIconImage(key = R.drawable.ic_marker, painter = painterResource(R.drawable.ic_marker))
+
+            if(markers.isNotEmpty()) {
+                PointAnnotation(
+                    point = markers[0]) {
+                        iconImage = marker
+                        iconSize = 0.05
+                    }
+            }
+        }
+
+        Icon(
+            painter = painterResource(id = R.drawable.crosshair),
+            contentDescription = "Map crosshair",
+            tint = Color.Black,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .size(20.dp)
         )
 
         if (hasPermission) {
@@ -119,7 +158,6 @@ fun MapboxMapScreen(locationViewModel: LocationViewModel) {
                             center(point)
                         }
                     } ?: run {
-                        // If null, try fetching again
                         locationViewModel.fetchLastLocation(context as ComponentActivity)
                     }
                 },
