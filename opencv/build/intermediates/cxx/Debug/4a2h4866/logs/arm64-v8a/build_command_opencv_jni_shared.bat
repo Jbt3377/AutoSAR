b0a33befe66ad1b473bce2bddfbbb392945cd0f5
Josh Beatty <beattyjoshua55@Gmail.com>
2025-10-28 15:54:06 +0000

    Remove OpenCV SDK from repository

--- opencv/build/intermediates/cxx/Debug/4a2h4866/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\BEATT\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\BEATT\\AndroidStudioProjects\\AutoSAR\\opencv\\.cxx\\Debug\\4a2h4866\\arm64-v8a" ^
-  opencv_jni_shared
--- opencv/build/intermediates/cxx/Debug/4a2h4866/logs/x86_64/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\BEATT\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\BEATT\\AndroidStudioProjects\\AutoSAR\\opencv\\.cxx\\Debug\\4a2h4866\\x86_64" ^
-  opencv_jni_shared
